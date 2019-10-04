b299228599d3380f76703b8fc009e4df3b0c7b7a
Mohsin <mohsin.binmurtada@gmail.com>
2019-10-04 19:48:41 +0800

    updated dummy data

--- database_init/002_insert_dummy_data.sql
+++ database_init/002_insert_dummy_data.sql
@@ -11,3 +11,46 @@ VALUES (
     'ABC',
     'XYZ'
 );
+
+INSERT INTO listings
+VALUES (
+    1,
+    'Product A',
+    'This is a sample product',
+    1
+);
+
+
+INSERT INTO listings
+VALUES (
+    2,
+    'Product B',
+    '50% discount',
+    1
+);
+
+
+INSERT INTO listings
+VALUES (
+    3,
+    'Mao Mao',
+    'Cat Child',
+    2
+);
+
+
+INSERT INTO listings
+VALUES (
+    4,
+    'Mew Mew',
+    'Cat',
+    2
+);
+
+INSERT INTO listings
+VALUES (
+    5,
+    'Dog',
+    'A cat called Dog',
+    2
+);
