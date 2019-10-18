8aeba0b5be16391cbab985b7f0de9ef57219351c
Mohsin <mohsin.binmurtada@gmail.com>
2019-10-18 20:07:14 +0800

    updated root index.js to allow capture of POST body

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const app = express();
 
 app.set('views', '/app/src/views/');
 app.set('view engine', 'ejs');
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use('/', routes);
 
 app.listen(3000, () => console.log('Server running on port 3000...'));
