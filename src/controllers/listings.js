48b48122ad2ef285314fc9b0965b4fd86f9f4a6f
Mohsin <mohsin.binmurtada@gmail.com>
2019-10-18 20:12:07 +0800

    added sample listings/create

--- src/controllers/listings.js
+++ src/controllers/listings.js
@@ -8,5 +8,15 @@ router.get('/', async function(request, response){
     response.render('listings/list.ejs', { listings: data });
 });
 
+router.get('/create', async function(request, response){
+    response.render('listings/create.ejs');
+});
+
+router.post('/create', async function(request, response){
+    const data = request.body;
+    console.log(data);
+    response.redirect('/listings/');
+});
+
 
 module.exports = router;
