aecbafd6a6c8b3e6736bb9cb8726d9875e57cc40
James <jamessoh11@gmail.com>
2019-10-19 16:30:03 +0800

    Added new create table SQL statement in database_init

---
+++ database_init/006_create_table_user_comment_on_listing.sql
@@ -0,0 +1,8 @@
+CREATE TABLE listing_comment(
+    id SERIAL PRIMARY KEY,
+    commented_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    comment VARCHAR(256) NOT NULL,
+    listing_id INT references listing(id) NOT NULL,
+    lister_id INT REFERENCES user_accounts(id) NOT NULL,
+    CONSTRAINT valid_rating CHECK( rating >= 0 AND rating <= 5 )
+);
