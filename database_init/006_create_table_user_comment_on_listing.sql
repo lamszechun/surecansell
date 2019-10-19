2c48caf7e019224cd158865f19699f959e7f59f2
James <jamessoh11@gmail.com>
2019-10-19 16:43:34 +0800

    More typo fixes in create table 006

--- database_init/006_create_table_user_comment_on_listing.sql
+++ database_init/006_create_table_user_comment_on_listing.sql
@@ -2,6 +2,6 @@ CREATE TABLE listing_comment(
     id SERIAL PRIMARY KEY,
     commented_at TIMESTAMP NOT NULL DEFAULT NOW(),
     comment VARCHAR(256) NOT NULL,
-    listing_id INT references listing(id) NOT NULL,
-    lister_id INT REFERENCES user_accounts(id) NOT NULL
+    commenter_id INT references user_accounts(id) NOT NULL,
+    listing_id INT references listing(id) NOT NULL
 );
