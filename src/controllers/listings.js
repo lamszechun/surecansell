c814313c671411309663813700037ffa2eeae2bb
Mohsin <mohsin.binmurtada@gmail.com>
2019-10-20 17:33:23 +0800

    added listings creation

--- src/controllers/listings.js
+++ src/controllers/listings.js
@@ -2,19 +2,37 @@ const express = require('express');
 const db = require('../../db');
 let router = express.Router();
 
-// URL: /listings/
+// all URL paths are prefixed with '/listings'
+
+// Get all our listings
 router.get('/', async function(request, response){
     const data = await db.query('SELECT * FROM listings', []);
     response.render('listings/list.ejs', { listings: data });
 });
 
+// Render our Listings Creation page
 router.get('/create', async function(request, response){
     response.render('listings/create.ejs');
 });
 
+// Use the input from the form to create a new listing
 router.post('/create', async function(request, response){
     const data = request.body;
-    console.log(data);
+
+    const title = data['title'];
+    const description = data['description'];
+    const price_in_cents = parseInt(data['price_in_dollars'])*100 + (parseInt(data['price_in_cents']) || 0);
+    const condition = data['condition'];
+
+    const result = await db.one(
+        'INSERT INTO listings' +
+        '(title, description, price_in_cents, condition, lister_id)' +
+        'VALUES($1,$2,$3,$4,$5)' +
+        'RETURNING id',
+        [title, description, price_in_cents, condition, 1]
+    );
+
+    console.log(result);
     response.redirect('/listings/');
 });
 
