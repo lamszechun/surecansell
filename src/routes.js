81a772b08797e9a36fed436c6deb249b8f5e1373
Mohsin <mohsin.binmurtada@gmail.com>
2019-10-22 21:47:54 +0800

    added my/listings

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const homepage = require('./controllers/homepage');
 const listings = require('./controllers/listings');
+const my = require('./controllers/my');
 
 let router = express.Router();
 
 router.use('/', homepage);
 router.use('/listings/', listings);
+router.use('/my/', my);
 
 module.exports = router;
