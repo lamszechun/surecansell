9f5601109a768ec725d5a4ed699393989c55884c
Mohsin <mohsin.binmurtada@gmail.com>
2019-10-23 00:57:35 +0800

    moved auth middleware to root app level

--- index.js
+++ index.js
@@ -1,7 +1,31 @@
 const express = require('express');
 const cookie_parser = require('cookie-parser');
-
 const routes = require('./src/routes');
+const db = require('./db');
+
+// middleware to get the current user from cookies
+const getCurrentUser = async function(request, response, next){
+    response.locals.user = null;
+    try {
+        const access_token = request.cookies.access_token;
+        if (access_token) {
+            response.locals.user = await db.oneOrNone(
+                'SELECT user_accounts.id,' +
+                '       user_accounts.username ' +
+                'FROM user_access_tokens ' +
+                'INNER JOIN user_accounts ' +
+                'ON user_access_tokens.user_id = user_accounts.id ' +
+                'WHERE user_access_tokens.value = $1',
+                [access_token]
+            );
+        }
+        next();
+    }
+    catch(error){
+        next(error);
+    }
+};
+
 const app = express();
 
 app.set('views', '/app/src/views/');
@@ -9,6 +33,7 @@ app.set('view engine', 'ejs');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookie_parser());
+app.use(getCurrentUser);
 app.use('/', routes);
 
 app.listen(3000, () => console.log('Server running on port 3000...'));
--- src/middleware.js
+++ src/middleware.js
@@ -1,19 +1,8 @@
-const db = require('../db');
-
 const signInRequired = async function (request, response, next) {
-    const access_token = request.cookies.access_token;
-    if(access_token){
-        const result = await db.oneOrNone(
-            'SELECT user_id FROM user_access_tokens ' +
-            'WHERE value = $1',
-            [access_token]
-        );
-        if(result){
-            response.locals.user_id = result['user_id'];
-            next();
-        }
+    if(response.locals.user){
+        next();
     }
-    else {
+    else{
         response.redirect('/sign-up/');
     }
 };
