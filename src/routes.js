1c0b230b217990ce99a8796eefb4c457426d8ce3
Mohsin <mohsin.binmurtada@gmail.com>
2019-10-23 19:49:14 +0800

    added seller endpoint

--- src/routes.js
+++ src/routes.js
@@ -2,11 +2,13 @@ const express = require('express');
 const homepage = require('./controllers/homepage');
 const listings = require('./controllers/listings');
 const my = require('./controllers/my');
+const seller = require('./controllers/seller');
 
 let router = express.Router();
 
 router.use('/', homepage);
 router.use('/listings/', listings);
 router.use('/my/', my);
+router.use('/seller/', seller);
 
 module.exports = router;
