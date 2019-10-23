4d847a9c36af2e42ad874034d24d28583f9236fd
Mohsin <mohsin.binmurtada@gmail.com>
2019-10-23 20:26:56 +0800

    added test_user by default when building the database

--- database_init/101_insert_dummy_user_accounts.sql
+++ database_init/101_insert_dummy_user_accounts.sql
@@ -6,7 +6,7 @@ VALUES (
         '+1 234 5678 90',
         'john.smith@gmail.com',
         'john_smith',
-        'nope_no_password'
+        '$2b$10$xiS.4vNwMnb6CrPiztOqx.sEqSIx/l6KNJx4T2IokiRRQvFqFTgDa'
 );
 
 INSERT INTO user_accounts
@@ -17,5 +17,20 @@ VALUES (
         '+2 783 4820',
         'alex.bjorkson@gmail.com',
         'alex_b',
-        'also_no_password'
+        '$2b$10$AxoCHEKQqOttpsvZ5HbwVudp/jZVZMUuFzOMcgUo4NTcyJ9/vxXQW'
 );
+
+-- Insert a test account for us to use for testing
+-- Username: test_user
+-- Password: password!
+INSERT INTO user_accounts
+(first_name, last_name, phone_number, email, username, encrypted_password)
+VALUES (
+           'Test',
+           'User',
+           '+1234567890',
+           'test.user@test.com',
+           'test_user',
+           '$2b$10$5AGvo/8gce03z6qTSZwwM.yVvFwcwEePi64.qfc4zy/lG/CXKXPeK'
+       );
+
