093404ea221a54702e7930226eed05db9cd8b740
Mohsin <mohsin.binmurtada@gmail.com>
2019-10-30 02:14:25 +0800

    added 404 page

--- src/routes.js
+++ src/routes.js
@@ -13,4 +13,9 @@ router.use('/my/', my);
 router.use('/seller/', seller);
 router.use('/chat/', chat);
 
+// this is our catch all to redirect the user to the 404 page
+router.get('*', async function(request, response){
+    response.redirect('/404/');
+});
+
 module.exports = router;
