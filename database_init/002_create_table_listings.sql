8e6420de64b221e3c16256f54d14c00c8732f647
Mohsin <mohsin.binmurtada@gmail.com>
2019-11-01 00:38:20 +0800

    updated listings table to include images & added default ones for the demo

--- database_init/002_create_table_listings.sql
+++ database_init/002_create_table_listings.sql
@@ -4,6 +4,7 @@ CREATE TABLE listings(
     description TEXT,
     price_in_cents BIGINT NOT NULL DEFAULT 0,
     condition VARCHAR(255) NOT NULL DEFAULT 'new',
+    image_file_path TEXT,
     lister_id INT REFERENCES user_accounts(id) NOT NULL,
     CONSTRAINT valid_price CHECK( price_in_cents >= 0 ),
     CONSTRAINT valid_conditions CHECK( condition = 'new' OR
--- database_init/202_insert_dummy_listings.sql
+++ database_init/202_insert_dummy_listings.sql
@@ -1,42 +1,22 @@
 INSERT INTO listings
-    (title, description, price_in_cents, condition, lister_id)
+(title, description, price_in_cents, condition, image_file_path, lister_id)
 VALUES (
-        'Product X',
-        'Flagship Product',
-        500,
-        'new',
-        5
-);
-
-
-INSERT INTO listings
-    (title, description, price_in_cents, condition, lister_id)
-VALUES (
-        'Product A - Sample',
-        'Try a sample of our newest item!',
-        0,
-        'new',
-        5
-);
-
-
-INSERT INTO listings
-    (title, description, price_in_cents, condition, lister_id)
-VALUES (
-        'Product A',
-        'Our new item! MUST BUY!',
-        1000,
-        'new',
-        5
-);
-
-
-INSERT INTO listings
-    (title, description, price_in_cents, condition, lister_id)
-VALUES (
-        'Cat',
-        'Name: Mao Mao',
-        100,
+        'Bicycle - Used Once',
+        '',
+        5000,
         'used',
+        '/static/images/default-demo/bicycle.jpg',
+        5
+);
+
+
+INSERT INTO listings
+(title, description, price_in_cents, condition, image_file_path, lister_id)
+VALUES (
+        'Yellow Van',
+        'Only drove 10,000 miles',
+        70000,
+        'used',
+        '/static/images/default-demo/van.jpg',
         6
 );
