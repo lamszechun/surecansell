af70a0810526c38bfc7fb86ded658327db2eaee4
Mohsin <mohsin.binmurtada@gmail.com>
2019-11-01 01:32:06 +0800

    added my/purchases & my/purchases/:id

--- src/controllers/my.js
+++ src/controllers/my.js
@@ -1,19 +1,69 @@
 const express = require('express');
 
 const db = require('../../db');
-const middleware = require('../middleware');
+const { signInRequired } = require('../middleware');
 
 let router = express.Router();
 
 
 // Get all the listings for the currently logged in user
-router.get('/listings', middleware.signInRequired, async function(request, response){
+router.get('/listings', signInRequired, async function(request, response){
     const data = await db.any(
         'SELECT * FROM listings ' +
         'WHERE lister_id = $1',
         [response.locals.user['id']]
     );
     response.render('my/listings.ejs', { listings: data });
+});
+
+// Purchase History
+router.get('/purchases', signInRequired, async function(request, response){
+    const data = await db.any(
+        'SELECT purchase_transactions.id AS transaction_id, ' +
+        '       purchase_transactions.price_in_cents, ' +
+        '       purchase_transactions.purchased_at, ' +
+        '       purchase_transactions.seller_id, ' +
+        '       listings.title, ' +
+        '       listings.image_file_path ' +
+        'FROM purchase_transactions ' +
+        'INNER JOIN listings ' +
+        'ON listings.id = purchase_transactions.listing_id ' +
+        'WHERE purchase_transactions.buyer_id = $1 ' +
+        'ORDER BY purchase_transactions.purchased_at DESC',
+        [response.locals.user['id']]
+    );
+    response.render('my/purchases.ejs', { purchases: data });
+});
+
+// Single Purchase
+router.get('/purchases/:id', signInRequired, async function(request, response){
+    const purchase_id = parseInt(request.params.id);
+
+    if(purchase_id){
+        const data = await db.oneOrNone(
+            'SELECT purchase_transactions.id AS transaction_id, ' +
+            '       purchase_transactions.price_in_cents, ' +
+            '       purchase_transactions.purchased_at, ' +
+            '       purchase_transactions.seller_id, ' +
+            '       listings.title, ' +
+            '       listings.image_file_path ' +
+            'FROM purchase_transactions ' +
+            'INNER JOIN listings ' +
+            'ON listings.id = purchase_transactions.listing_id ' +
+            'WHERE purchase_transactions.id = $1' +
+            '  AND purchase_transactions.buyer_id = $2',
+            [purchase_id, response.locals.user['id']]
+        );
+        if(data){
+            response.render('my/purchases_detail.ejs', { purchase: data });
+        }
+        else{
+            response.redirect('/my/purchases/');
+        }
+    }
+    else{
+        response.redirect('/my/purchases/');
+    }
 });
 
 
@@ -23,7 +73,7 @@ router.get('/listings', middleware.signInRequired, async function(request, respo
 
 //Post listing Review -- NOT SURE about the id portion
 
-router.post('/listing_reviews', middleware.signInRequired, async function(request, response){
+router.post('/listing_reviews', signInRequired, async function(request, response){
     const data = await request.body;
 
     const listing_id = data['listing_id'];
@@ -46,7 +96,7 @@ router.post('/listing_reviews', middleware.signInRequired, async function(reques
 });
 
 // Get all the reviews WRITTEN BY the logged in user.
-router.get('/listing_reviews', middleware.signInRequired, async function(request, response){
+router.get('/listing_reviews', signInRequired, async function(request, response){
     const data = await db.any(
         'SELECT * FROM listings_review ' +
         'WHERE user_id = $1',
@@ -57,7 +107,7 @@ router.get('/listing_reviews', middleware.signInRequired, async function(request
 
 
 // Get all the reviews ABOUT the logged in user
-router.get('/listing_reviews', middleware.signInRequired, async function(request, response){
+router.get('/listing_reviews', signInRequired, async function(request, response){
     const data = await db.any(
         'SELECT * FROM listings_review, listings ' +
         'WHERE listings_review.listing_id = listings.id ' +
