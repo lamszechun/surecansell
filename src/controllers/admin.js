af6d20b037d1c4eadc1416aae6b0717a0be163ac
lamszechun <zcnmashleu@gmail.com>
2019-11-08 14:10:44 +0800

    Add files via upload

--- src/controllers/admin.js
+++ src/controllers/admin.js
@@ -14,8 +14,34 @@ router.get('/', adminRequired, async function(request, response){
 
 // Admin Analytics Page
 router.get('/analytics', adminRequired, async function(request, response){
-    response.render('admin/analytics.ejs');
+    const total_purchases = await db.query(
+        "SELECT COUNT(*)" +
+        "FROM purchase_transactions",
+        []
+    );
+
+    const each_spending = await db.query(
+        "SELECT purchase_transactions.buyer_id, SUM (purchase_transactions.price_in_cents)" +
+        "FROM purchase_transactions" +
+        "GROUP BY purchase_transactions.buyer_id" +
+        "ORDER BY SUM (purchase_transactions.price_in_cents) DESC",
+        []
+    );
+
+    const most_expensive = await db.query(
+        "SELECT MAX (listings.price_in_cents)" +
+        "FROM listings l",
+        []
+    );
+
+    response.render('admin/analytics.ejs', {purchases: total_purchases}, {spending: each_spending}, {expensive: most_expensive});
 });
+//Purchase Transactions where the buyer and seller have the same last name
+//SELECT p.id
+//FROM purchase_transactions p, user_accounts u, user_accounts u1
+//WHERE p.buyer_id = u.id AND u.last_name = u1.last_name AND p.seller_id = u1.id AND u.id < u1.id ;
+
+
 
 
 // Admin Listings Page
